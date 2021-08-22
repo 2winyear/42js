b511ff0ef80e095ed69e4d5d7c241ed18be55e9f
seungyel <lsy2246@naver.com>
2021-08-23 08:35:05 +0900

    error ex00

--- fe02/ex00/src/App.js
+++ fe02/ex00/src/App.js
@@ -1,30 +1,40 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import { userInfo } from 'os';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 const App = () => {
-    const getUsers = async() => {
-      try {
-        const response = await axios({
-          method: "get",
-          url: "https://cors.bridged.cc/https://api.notion.com/v1/users/",
-          headers: {
-            "Authorition": process.env.development.REACT_APP_NOTION_DATABASE_ID,
-            "Notion-Version": "2021-08-16"
-          }
-      })
-      console.log(response.url);
-      } catch (e) {
-        console.log('error');
-        console.log(e);
-      }
-    }
-    getUsers();
-    return (
-      <ul>
-        {userInfo}
-      </ul>
-    )
-}
+	const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
+	useEffect(() => {
+		const getApi = async () => {
+			const url = `https://cors.bridged.cc/https://api.notion.com/users/:id`;
+			try {
+        setError(null);
+        setData(null);
+        console.log("요청!")
+				const response = await axios.get(url, {
+					headers: {
+						"Notion-Version": "2021-08-16",
+						"Authorization": `Bearer ${process.env.REACT_APP_NOTION_KEY}`,
+					}
+				});
+        setData(response);
+        console.log(response.data);
+        // console.log(response());
+        // console.log(response.name);
+			} catch (error) {
+        // console.log(error);
+        setError(error);
+        console.log('으악');
+			}
+		}
+		getApi();
+	}, []);
+
+	return (
+		<div>
+      hi
+    </div>
+	)
+}
 export default App;
